6d0035c6603ab122b328a6a9526ca6fb625f0621
dattasaitejat <84062887+dattasaitejat@users.noreply.github.com>
2021-06-07 10:10:51 +0530

    Add files via upload

---
+++ factoriatbydynammic.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int dp[1000];
+int fact(int n){
+    if((n==0)|(n==1)){
+        return 1;
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    return (dp[n]=n*fact(n-1));
+}
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++) dp[i]=-1;
+    cout<<fact(n);
+    for(int i=0;i<n;i++) cout<<dp[i]<<" ";
+    return 0;
+}
